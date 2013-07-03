9b99070d7fefcd78a6be09243a6b19fec7f9ed2e
Ubuntu <ubuntu@ip-10-112-223-147.ec2.internal>
2013-07-03 06:19:26 +0000

    iteswq:

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/output',function(request,response) {
+  response.send('testing');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
